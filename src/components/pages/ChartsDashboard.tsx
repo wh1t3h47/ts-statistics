bb88c911cf30a57458969d57b8cf5f52af411efe
wh1t3h47 <tom.mharres@gmail.com>
2023-10-15 16:01:49 -0300

    fix: padrões de código/ DRY :wrench:

--- src/components/pages/ChartsDashboard.tsx
+++ src/components/pages/ChartsDashboard.tsx
@@ -15,46 +15,56 @@ const ChartsDashboard = () => {
     useState<JSX.Element | null>(null);
 
   useEffect(() => {
+    const errHandler = (error: Error) => {
+      console.error(error);
+      setImportLoading(false);
+    };
+
     import("../organisms/PartyDistributionBarChart")
       .then((module) => {
         setPartyDistributionChart(<module.PartyDistributionBarChart />);
       })
       .then(() => {
-        import("../organisms/TopContributingBarChart").then((module) => {
-          setTopContributingChart(<module.TopContributingBarChart />);
-        });
+        import("../organisms/TopContributingBarChart")
+          .then((module) => {
+            setTopContributingChart(<module.TopContributingBarChart />);
+          })
+          .catch(errHandler);
       })
       .then(() => {
-        import("../organisms/BitcoinNegotiationsParetoChart").then((module) => {
-          setBitcoinNegotiationsChart(
-            <module.BitcoinNegotiationsParetoChart />
-          );
-          setImportLoading(false);
-        });
+        import("../organisms/BitcoinNegotiationsParetoChart")
+          .then((module) => {
+            setBitcoinNegotiationsChart(
+              <module.BitcoinNegotiationsParetoChart />
+            );
+            setImportLoading(false);
+          })
+          .catch(errHandler);
       })
-      .catch((error) => {
-        console.error(error);
-        setImportLoading(false);
-      });
+      .catch(errHandler);
   }, []);
 
-  const isLoading = importLoading || loading || btcLoading;
+  const isLoadingSenator = importLoading || loading;
+  const isLoadingBitcoin = importLoading || btcLoading;
 
   return (
     <div className="flex flex-wrap justify-center">
       <Card
         title="Distribuição de partidos ocupando
             posições no senado (EUA):"
-        {...{ isLoading }}
+        {...{ isLoading: isLoadingSenator }}
       >
         {partyDistributionChart}
       </Card>
-      <Card title="Top 5 contribuidores no senado (EUA):" {...{ isLoading }}>
+      <Card
+        title="Top 5 contribuidores no senado (EUA):"
+        {...{ isLoading: isLoadingSenator }}
+      >
         {topContributingChart}
       </Card>
       <Card
         title="Distribuição de volume de negociações por criptomoeda:"
-        {...{ isLoading }}
+        {...{ isLoading: isLoadingBitcoin }}
       >
         {bitcoinNegotiationsChart}
       </Card>
